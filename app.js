4cf58a024589ee64fcc5a3274a195450d514a0b8
Jordan Pufon <fabovepufonjordan@gmail.com>
2023-06-08 15:48:41 -0500

    change port to 3000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-const port = 5432;
+const port = 3000;
 const passport = require('passport');
 require('./auth/passport-config')(passport)
 
